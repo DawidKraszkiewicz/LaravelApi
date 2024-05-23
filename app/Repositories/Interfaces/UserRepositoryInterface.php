1cec98af7bb5246f467f91760f25a4a0a5e0c729
Dawid Kraszkiewicz <dawid.kraszkiewicz@logicnet.pl>
2024-05-23 20:16:05 +0200

    refactor(users): added strict_types

--- app/Repositories/Interfaces/UserRepositoryInterface.php
+++ app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -8,9 +8,9 @@ interface UserRepositoryInterface
 
     public function create(array $data);
 
-    public function update(array $data, $id);
+    public function update(array $data, int $id);
 
-    public function delete($id);
+    public function delete(int $id);
 
-    public function find($id);
+    public function find(int $id);
 }
