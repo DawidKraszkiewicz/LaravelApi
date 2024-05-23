542628dd8ce77988253c41cf205d0f52c85449af
Dawid Kraszkiewicz <dawid.kraszkiewicz@logicnet.pl>
2024-05-23 20:16:38 +0200

    fix(user): fixed namespace and added strict type

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -1,33 +1,33 @@
 <?php
-namespace App\Repositories;
+declare(strict_types=1);
+namespace App\Repositories\User;
 use App\Models\User;
 use App\Repositories\Interfaces\UserRepositoryInterface;
 
 class UserRepository implements UserRepositoryInterface {
-    public function all()
-    {
+    public function all(): \Illuminate\Database\Eloquent\Collection {
         return User::all();
     }
 
-    public function create(array $data)
+    public function create(array $data): User
     {
         return User::create($data);
     }
 
-    public function update(array $data, $id)
+    public function update(array $data, int $id): User
     {
         $user = User::findOrFail($id);
         $user->update($data);
         return $user;
     }
 
-    public function delete($id)
+    public function delete(int $id): void
     {
         $user = User::findOrFail($id);
         $user->delete();
     }
 
-    public function find($id)
+    public function find(int $id): User
     {
         return User::findOrFail($id);
     }
