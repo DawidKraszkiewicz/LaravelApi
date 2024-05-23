4c97bbcab305cb6eda848bd24395d6940525b856
Dawid Kraszkiewicz <dawid.kraszkiewicz@logicnet.pl>
2024-05-23 20:14:04 +0200

    refactor(users): added validation

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -3,7 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 class UserUpdateRequest extends FormRequest
 {
     /**
@@ -26,5 +27,17 @@ class UserUpdateRequest extends FormRequest
             'email' => 'required|unique:users,email,' . $this->route('id'),
             'password' => 'required|confirmed',
         ];
+    }
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        redirect()->back()->withInput()->withErrors($validator);
     }
 }
