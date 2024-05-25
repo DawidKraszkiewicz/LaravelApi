9a847eb4e4f26e1b140c1c4c21bdb153f614219a
DawidKraszkiewicz <kraszka1234@gmail.com>
2024-05-25 20:27:50 +0200

    refactor(user): moved validation to UserCreateRequest.php

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\UserUpdateRequest;
+use App\Http\Requests\User\UserCreateRequest;
+use App\Http\Requests\User\UserUpdateRequest;
 use App\Services\User\UserService;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -17,31 +17,27 @@ class UserController extends Controller {
     }
     public function index(): View {
         $users = $this->userService->all();
+
         return view('users.index', compact('users'));
     }
     public function create(): View {
         return view('users.create');
     }
-    public function store(Request $request): RedirectResponse
+    public function store(UserCreateRequest $request): RedirectResponse
     {
-        $data = $request->validate([
-            'name' => 'required',
-            'email' => 'required|unique:users,email',
-            'password' => 'required|confirmed'
-        ]);
-
+        $data = $request->validated();
         $user = $this->userService->create($data);
 
         return redirect()->route('users.index', $user->id);
     }
     public function edit(int $id): View {
         $user = $this->userService->find($id);
+
         return view('users.edit', compact('user'));
     }
     public function update(UserUpdateRequest $request, int $id): RedirectResponse
     {
         $data = $request->validated();
-
         $user = $this->userService->update($data, $id);
 
         return redirect()->route('users.edit', $user->id)->with('success', 'User updated successfully');
