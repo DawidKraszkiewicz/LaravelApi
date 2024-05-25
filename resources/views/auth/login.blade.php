b38845770d5ebf4fd2e00478bc8eddd32ede8c1d
DawidKraszkiewicz <kraszka1234@gmail.com>
2024-05-25 20:31:40 +0200

    feature(routes): changes to views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,8 @@
 <form action="{{route('auth.login.submit')}}" method="POST">
     @csrf
+    <label for="email">Email</label>
     <input type="email" name="email">
+    <label for="password">Password</label>
     <input type="password" name="password">
     <button type="submit">Login</button>
 </form>
