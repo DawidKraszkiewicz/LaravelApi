e8f3155a1264435c4b1f5518aac3d69069c001d9
DawidKraszkiewicz <kraszka1234@gmail.com>
2024-05-25 20:28:17 +0200

    feature(user): Request For user creation

---
+++ app/Http/Requests/User/UserCreateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required|confirmed',
+            'role' => 'required|in:0,1|integer'
+        ];
+    }
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        redirect()->back()->withInput()->withErrors($validator);
+    }
+}
