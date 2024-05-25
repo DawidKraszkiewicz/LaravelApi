4d4ff3a6a8c6f084268116a2595e4e393bb2cb4c
DawidKraszkiewicz <kraszka1234@gmail.com>
2024-05-25 20:28:34 +0200

    feature(auth): Request For user login

--- app/Http/Requests/User/UserLoginRequest.php
+++ app/Http/Requests/User/UserLoginRequest.php
@@ -1,1 +1,36 @@
 <?php
+declare(strict_types=1);
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class UserLoginRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ];
+    }
+
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $user = User::where('email', $this->email)->first();
+
+            if (!$user || !Hash::check($this->password, $user->password)) {
+                $validator->errors()->add('email', 'The provided credentials are incorrect.');
+            }
+        });
+    }
+}
